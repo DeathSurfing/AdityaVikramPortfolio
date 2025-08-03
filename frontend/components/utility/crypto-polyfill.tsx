1842790a5dcb5a256488fb345b4f5645fb74a59a
Aditya Vikram Mahendru <70932037+DeathSurfing@users.noreply.github.com>
2025-08-03 15:30:38 +0530

    Update crypto-polyfill.tsx

--- frontend/components/utility/crypto-polyfill.tsx
+++ frontend/components/utility/crypto-polyfill.tsx
@@ -10,13 +10,15 @@ export default function CryptoPolyfill() {
       }
 
       if (!window.crypto.randomUUID) {
-        window.crypto.randomUUID = function (): string {
+        const fallbackUUID = function (): string {
           return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             const r = Math.random() * 16 | 0;
             const v = c === 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
           });
         };
+
+        window.crypto.randomUUID = fallbackUUID as unknown as typeof crypto.randomUUID;
       }
     }
   }, []);
